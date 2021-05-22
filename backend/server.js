6ddc728da4a82329a54a73168b12e38e76cd6727
kabir-02 <55957253+kabir-02@users.noreply.github.com>
2021-05-22 23:03:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const mysql = require('mysql');
 
 
-PORT = 8084;
+PORT = 8082;
 
 let db = mysql.createConnection({
   host: 'localhost',
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
     console.log("Connected!");
     
   });
-  res.send('hello KK');
+  res.send('hello world');
 });
   
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
