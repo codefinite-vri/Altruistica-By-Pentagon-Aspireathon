db213dee939052f1a031fae218a3be252572b5ca
kabirkararia <46525953+kabirkararia@users.noreply.github.com>
2021-05-22 22:55:01 +0530

    Database created with name 'funddb'

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
-PORT = 8082;
+const mysql = require('mysql');
+
+
+PORT = 8084;
+
+let db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'funddb'
+});
+
   
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    
+  });
+  res.send('hello KK');
 });
   
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
